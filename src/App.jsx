45d456e78210b14d3aec2632a831fa7542ae00ea
RoxanaRoxana <84925329+RoxanaRoxana@users.noreply.github.com>
2022-03-13 19:30:11 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 import styles from './App.module.css';
 
+
 export default function App() {
   return (
     <div className={styles.App}>
